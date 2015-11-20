bbc57599afec54d641a990e28ed5f72f9471e8e1
Lesha Koss <regiusprod@gmail.com>
2015-11-20 20:21:00 +0600

    class Ui

---
+++ android/ui/index.js
@@ -0,0 +1,17 @@
+import React from 'react-native'
+import style from './style'
+
+const {Text, View} = React
+
+export default class InstagramForSounds extends React.Component {
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={style.poop}>
+          💩
+        </Text>
+      </View>
+    );
+  }
+}
+
---
+++ android/ui/style.js
@@ -0,0 +1,17 @@
+import React from 'react-native'
+
+export default React.StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  poop: {
+    fontSize: 100,
+    textAlign: 'center',
+    margin: 10,
+  },
+})
+
